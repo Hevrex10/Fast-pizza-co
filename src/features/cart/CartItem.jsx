2171a78f0fb7228e0afdc98cc2db878698bde257
hevrex10 <eadeagbo110@gmail.com>
2025-11-26 13:05:55 +0100

    Updated fast-pizza-co with new changes

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -2,7 +2,7 @@ import { formatCurrency } from '../../utils/helpers';
 import { useSelector } from 'react-redux';
 import { getCurrentQuantityById} from './cartSlice'
 import DeleteItem from './DeleteItem';
-import UpdateItemQuantity from './UPdateItemQuantity';
+import UPdateItemQuantity from './UPdateItemQuantity';
 
 
 function CartItem({ item }) {
@@ -17,7 +17,7 @@ function CartItem({ item }) {
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-          <UpdateItemQuantity pizzaId={pizzaId} currentQuantity={currentQuantity}/>
+          <UPdateItemQuantity pizzaId={pizzaId} currentQuantity={currentQuantity}/>
            <DeleteItem id={pizzaId} />
       </div>
     </li>
